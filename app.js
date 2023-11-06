413eaf5b1814d26881992e2a6cdb244817c6ad3a
Viniyarda <vinitayarda06@gmail.com>
2023-11-06 21:30:10 +0530

    completed

---
+++ app.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+var cors = require('cors');
+const path = require('path');
+
+
+
+
+// import routes
+const authRoutes = require('./routes/authRoutes');
+const userRoutes = require('./routes/userRoutes');
+const jobTypeRoute = require('./routes/jobsTypeRoutes');
+const jobRoute = require('./routes/jobsRoutes');
+
+const cookieParser = require("cookie-parser");
+const errorHandler = require("./middleware/error");
+
+//database connection
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log(err));
+
+//MIDDLEWARE
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'))
+}
+app.use(bodyParser.json({ limit: "5mb" }));
+app.use(bodyParser.urlencoded({
+  limit: "5mb",
+  extended: true
+}));
+app.use(cookieParser());
+app.use(cors());
+
+
+//ROUTES MIDDLEWARE
+// app.get('/', (req, res) => {
+//     res.send("Hello from Node Js");
+// })
+app.use('/api', authRoutes);
+app.use('/api', userRoutes);
+app.use('/api', jobTypeRoute);
+app.use('/api', jobRoute);
+
+__dirname = path.resolve()
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
+
+// error middleware
+app.use(errorHandler);
+
+//port
+const port = process.env.PORT || 9000
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
